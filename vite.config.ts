d35dc8090e716a319230e4fca320b77d854c7f21
Clinton clinton_wee <clinton_wee@hotmail.com>
2026-02-08 11:07:59 +0000

    Another attempt

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,11 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 import path from 'path'
+import { viteCommonjs } from '@originjs/vite-plugin-commonjs'
 
 // https://vite.dev/config/
 export default defineConfig({
-    plugins: [react(), tailwindcss()],
+    plugins: [react(), tailwindcss(), viteCommonjs()],
     resolve: {
         alias: {
             '@': path.resolve(__dirname, './src'),
